afae785be467b82770a9a25460a161c82ab4f183
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-01-05 11:17:50 +0100

    Add files via upload

---
+++ codes/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+	public static void main(String[] args) {
+		  int i;
+		  double x, y;
+		  String z = "Hallo Welt";
+		  i = 6;
+		  x = i * 2;
+		  y = 9 / x;
+		  System.out.print("Der Wert des Integers \"i\" beträgt: ");
+		  System.out.println(i);
+		  System.out.print("Der Wert von x beträgt: ");
+		  System.out.println(x);
+		  System.out.print("Der Wert von y beträgt: ");
+		  System.out.println(y);
+		  System.out.print("Der Wert von z beträgt: ");
+		  System.out.println(z);
+		 }
+}
---
+++ codes/ifstatement.java
@@ -0,0 +1,13 @@
+public class ifstatement {
+	public static void main(String[] args) {
+		int a, b;
+		a = 3;
+		b = 5;
+		if (a * b == 15) {
+			System.out.print("3 * 5 == 15! 👍");
+		}
+		else {
+			System.out.print("3 * 5 == 15! 👎");
+		}
+	}
+}
