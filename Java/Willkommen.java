6843212554a7a000432a4dd30acb19eedfc26a30
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-01-05 16:30:32 +0100

    Add files via upload

---
+++ Java/Willkommen.java
@@ -0,0 +1,16 @@
+/*
+Denke daran, dass die Klasse identisch zum Dateinamen sein muss. In dem Fall heiﬂt diese Datei "Willkommen.java", weshalb man als Class Namen
+"Willkommen" benutzen muss, sonst kommt es zu folgendem Fehler: "The public type willkommen must be defined in its own file"
+*/
+
+public class Willkommen {
+
+ public static void main(String[] args) {
+  /*
+   * Um mehrere Zeilen in Java auszugeben, ist die Nutzung von System.out.println
+   * hilfreich und ratsam
+   */
+  System.out.println("Hello World");
+  System.out.println("Family is Key to success");
+ }
+}
