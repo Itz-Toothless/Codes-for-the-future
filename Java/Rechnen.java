2dde3508c7ffb472a1854ce2205b4839c515fbb9
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-01-25 14:29:24 +0100

    Update Rechnen.java

--- Java/Rechnen.java
+++ Java/Rechnen.java
@@ -1,5 +1,11 @@
 public class Rechnen {
 	public static void main(String[] args) {
+		/* 
+			Initialisierung der Variablen a und b als floats. (Dieser Datentyp wird für die
+			Darstellung einer reellen Zahl mit der Größe von 4 byte (32 bit) benutzt.)
+			Alternativ können die Variablen a und b als double initialisiert werden. (Dieser Datentyp wird für die
+			Darstellung einer reellen Zahl mit der Größe von 8 byte (64 bit) benutzt.)
+		*/
 		float a, b;
 		a = 0.35f;
 		b = 5f;
