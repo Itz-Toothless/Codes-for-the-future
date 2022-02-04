711dcdcdbc4d36722f545e160c3ae3902fd1f5a0
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-02-04 23:39:27 +0100

    Update password-gen.py

--- Python/password-gen.py
+++ Python/password-gen.py
@@ -6,7 +6,7 @@ chars = string.ascii_letters + string.digits + string.printable  # Definition de
 number = int(input("Bitte gebe die Länge des Passworts an: "))  # Empfehlung für die Länge: Mindestens 8 Zeichen
 for _ in range(number):
     pw = ''.join(random.choice(chars) for _ in range(number))
-print(f"Das Passwort '{pw}' wurde in die Datei 'passwords.txt' eingetragen")
-with open('passwords.txt' , 'a') as f:  #  Optional: Speichern des Passwortes in eine Datei
-    f.write(pw)
-    f.write("\n")
+    print(f"Das Passwort '{pw}' wurde in die Datei 'passwords.txt' eingetragen")
+    with open('passwords.txt' , 'a') as f:  #  Optional: Speichern des Passwortes in eine Datei
+        f.write(pw)
+        f.write("\n")
