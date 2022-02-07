2a35905304f3e35496154b2c69a8cd4a2d877745
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-02-07 01:45:52 +0100

    Update while-loop.c

--- C/while-loop.c
+++ C/while-loop.c
@@ -2,12 +2,12 @@
 
 int main()
 {
+ 
  int i = 0;
- while (i <= 100)
+ while (i <= 100) // Lässt die Variable "i", welche einen Integer darstellt, den Wert 100 nicht übersteigen. 
  {
-  printf("Zahl: %i\n", i);
-  /* Hello World */
-  i++;
+  printf("Zahl: %i\n", i); // Gibt die Zahl aus, an dieser sich i jetzt befindet.
+  i++; // Addiert zu i 1 dazu, bis die 100 erreicht wurde
  }
- return 0;
+ return 0; // Wenn das Programm die Zahl 100 erreicht hat, wird als return der Wert 0 ausgegeben, welcher bedeutet, dass das Programm ordnungsgemäß und erwartet ausgeführt wurde
 }
