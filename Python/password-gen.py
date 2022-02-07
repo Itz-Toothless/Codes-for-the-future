0631db2b4ecdb67aeb1afd069b16d332187fbac0
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-02-07 02:26:12 +0100

    Update password-gen.py

--- Python/password-gen.py
+++ Python/password-gen.py
@@ -3,10 +3,14 @@ import random
 
 chars = string.ascii_letters + string.digits + string.printable  # Definition der String-Argumente
 
-number = int(input("Bitte gebe die Länge des Passworts an: "))  # Empfehlung für die Länge: Mindestens 12 Zeichen
+number = int(input("Bitte gebe die Länge des Passworts an: "))  
+"""
+Bittet den Nutzer um die Eingabe eines Integers, welche die Länge des Passworts darstellt.
+
+Empfehlung für die Länge eines Passwortes: Mindestens 12 Zeichen
+"""
 for _ in range(number):
     pw = ''.join(random.choice(chars) for _ in range(number))
     print(f"Das Passwort '{pw}' wurde in die Datei 'passwords.txt' eingetragen")
     with open('passwords.txt' , 'a') as f:  #  Optional: Speichern des Passwortes in eine Datei
-        f.write(pw)
-        f.write("\n")
+        f.write(f"{pw}\n")
