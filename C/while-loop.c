9fcf0c41ad76abbcfd001ce62072c538d483f889
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-02-07 01:46:07 +0100

    Update while-loop.c

--- C/while-loop.c
+++ C/while-loop.c
@@ -9,5 +9,5 @@ int main()
   printf("Zahl: %i\n", i); // Gibt die Zahl aus, an dieser sich i jetzt befindet.
   i++; // Addiert zu i 1 dazu, bis die 100 erreicht wurde
  }
- return 0; // Wenn das Programm die Zahl 100 erreicht hat, wird als return der Wert 0 ausgegeben, welcher bedeutet, dass das Programm ordnungsgemäß und erwartet ausgeführt wurde
+ return 0; // Wenn i die Zahl 100 erreicht hat, wird als return der Wert 0 ausgegeben, welcher bedeutet, dass das Programm ordnungsgemäß und erwartet ausgeführt wurde
 }
