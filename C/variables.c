e1c9668d9dd05094c8b814199a17c76a49c20e70
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-03-16 18:38:13 +0100

    Delete variables.c

--- C/variables.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
- int zahl1 = 8;
- int zahl2 = 8;
- char* args = "Hallo Welt, ich heiße Ohnezahn^^";
- printf("String: %s\n\nZahl: %i", args, zahl1 + zahl2);
- return 0;
-}
