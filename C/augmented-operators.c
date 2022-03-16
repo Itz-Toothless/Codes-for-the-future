4c1e7e50ee103bae9d83f5a8329ca1fc08552f86
Ohnezahn ZAE <80362356+Ohnezahn-ZAE@users.noreply.github.com>
2022-03-16 18:31:59 +0100

    Update augmented-operators.c

--- C/augmented-operators.c
+++ C/augmented-operators.c
@@ -2,11 +2,11 @@
 
 int main() {
  int i = 10;
- // ! i+=2; (used for Addition)
- // ! i-=2; (used for subtraction)
- // ! i*=2; (used for Multiplying)
- // ! i/=2; (used for Division)
- // ! i%=2; (used for Modulus)
+ // i+=2; (used for Addition)
+ // i-=2; (used for subtraction)
+ // i*=2; (used for Multiplying)
+ // i/=2; (used for Division)
+ // i%=2; (used for Modulus)
 
  int absolute_result = i*10;
  printf("%d", absolute_result);
